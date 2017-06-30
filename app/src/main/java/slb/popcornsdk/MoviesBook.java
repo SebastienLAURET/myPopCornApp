16c5fd4ef7f598ffc275ad35241178242f693d43
lauret_s <sebastien.lauret@epitech.eu>
2017-06-30 21:20:59 +0200

    create movie controller

--- app/src/main/java/slb/popcornsdk/MoviesBook.java
+++ app/src/main/java/slb/popcornsdk/MoviesBook.java
@@ -1,8 +1,40 @@
 package slb.popcornsdk;
 
+import java.util.ArrayList;
+
+import slb.popcornsdk.data.movie.Movie;
+import slb.popcornsdk.requesthandler.MovieRequestHandle;
+
 /**
  * Created by lauret_s on 30/06/2017.
  */
 
 public class MoviesBook {
+    private MovieRequestHandle _requestHandel = new MovieRequestHandle();
+    private ArrayList<String> _pageName;
+    private int _actPage = 0;
+    private ArrayList<Movie> _movies;
+
+    public MoviesBook() {
+        _pageName = _requestHandel.getNamePages();
+        loadPage();
+    }
+    public ArrayList<Movie> getMovies() {
+        return _movies;
+    }
+    public ArrayList<Movie> nextPage() {
+        if (_actPage + 1 < _pageName.size())
+            ++_actPage;
+        loadPage();
+        return _movies;
+    }
+    public ArrayList<Movie> prevPage() {
+        if (_actPage - 1 >= 0)
+            --_actPage;
+        loadPage();
+        return _movies;
+    }
+    private void loadPage() {
+        _movies = _requestHandel.getPage(_pageName.get(_actPage));
+    }
 }
