a62e3b212899f5bf770d301613166606f1fa3681
lauret_s <sebastien.lauret@epitech.eu>
2017-06-30 21:19:55 +0200

    remove test MainActivity page

--- app/src/main/java/slb/mypopcornapp/MainActivity.java
+++ app/src/main/java/slb/mypopcornapp/MainActivity.java
@@ -2,15 +2,6 @@ package slb.mypopcornapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-
-import java.util.ArrayList;
-
-import slb.popcornsdk.data.movie.Movie;
-import slb.popcornsdk.requestbuilder.RequestBuilderMovies;
-import slb.popcornsdk.requesthandler.ARequestHandle;
-import slb.popcornsdk.requesthandler.MovieRequestHandle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MovieRequestHandle requestHandel = new MovieRequestHandle();
-        ArrayList<String> res = requestHandel.getNamePages();
-        ArrayList<Movie> movies =  requestHandel.getPage(res.get(0));
-        Log.d("RESULT", movies.toString());
     }
 }
